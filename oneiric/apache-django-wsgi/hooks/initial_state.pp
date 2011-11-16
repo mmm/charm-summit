338b84627b33e1b5ebd63818d71d9ecda819177b
Michael Nelson <michael.nelson@canonical.com>
2011-11-16 17:22:24 +0000

    Savepoint - link in project_layout not yet working.

--- oneiric/apache-django-wsgi/hooks/initial_state.pp
+++ oneiric/apache-django-wsgi/hooks/initial_state.pp
@@ -12,16 +12,18 @@ file { "/srv/$::service_hostname/":
   ensure => directory,
 }
 
-vcsrepo {"/tmp/django-app-branch":
-  ensure => present,
-  provider => $::app_source_type,
-  source   => $::app_source,
-  revision => $::app_source_revision,
-}
-# Using `pip install` together with vcsrepo here as `pip -e ...` to get the
-# branch directly won't install it on the system.
-package {"/tmp/django-app-branch":
-  provider => pip,
-  ensure   => installed,
-  require  => Vcsrepo["/tmp/django-app-branch"],
+if $::app_source {
+  vcsrepo {"/tmp/django-app-branch":
+    ensure => present,
+    provider => $::app_source_type,
+    source   => $::app_source,
+    revision => $::app_source_revision,
+  }
+  # Using `pip install` together with vcsrepo here as `pip -e ...` to get the
+  # branch directly won't install it on the system.
+  package {"/tmp/django-app-branch":
+    provider => pip,
+    ensure   => installed,
+    require  => Vcsrepo["/tmp/django-app-branch"],
+  }
 }

2fe196b89fb030a10335ec084c9fdcb06895b382
Michael Nelson <michael.nelson@canonical.com>
2011-11-22 11:53:10 +0000

    Added static file handling for apache.

--- oneiric/apachedjangowsgi/hooks/manifests/database_settings.pp
+++ oneiric/apachedjangowsgi/hooks/manifests/database_settings.pp
@@ -3,3 +3,21 @@ file { "/srv/$::service_hostname/project/local.cfg":
   content => template("apache-django-wsgi/local.cfg.erb"),
 }
 
+exec { "python ./manage.py syncdb --noinput":
+  cwd => "/srv/$::service_hostname/project",
+  path => ["/usr/bin"],
+  require => File["/srv/$::service_hostname/project/local.cfg"],
+}
+exec { "python ./manage.py migrate":
+  cwd => "/srv/$::service_hostname/project",
+  path => ["/usr/bin"],
+  require => Exec["python ./manage.py syncdb --noinput"],
+}
+
+# Only after we're certain that all required apps/packages are installed
+# do we collect all the static files ready for apache to serve.
+exec { "python ./manage.py collectstatic --noinput":
+  cwd => "/srv/$::service_hostname/project",
+  path => ["/usr/bin"],
+  require => File["/srv/$::service_hostname/project/local.cfg"],
+}
